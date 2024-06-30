609e89b2413d2eded045a20e6cc1cd7048f08127
Kajetan Stach <kajetan.stach@microsoft.wsei.edu.pl>
2024-06-30 21:35:36 +0200

    val4

--- Projekt zaliczeniowy/Program.cs
+++ Projekt zaliczeniowy/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<ZooDbContext>(options =>
 // Add services to the container.
 builder.Services.AddRazorPages();
 var app = builder.Build();
-
+//
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
